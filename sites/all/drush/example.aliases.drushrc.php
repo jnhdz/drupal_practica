d9eb3b7e8d7d6d963ab43b7768c144a858c9cd7b
jnhdz <jenhrdz@gmail.com>
2014-11-05 16:00:31 -0600

    rp1

---
+++ sites/all/drush/example.aliases.drushrc.php
@@ -0,0 +1,24 @@
+<?php
+$aliases['gonet.mncrmtc'] = array (
+  'remote-host' => 'gonet.mncrmtc.com',
+  'remote-user' => 'gonet',
+  'root' => '/public_html/jennyh',
+  'uri' => 'http://gonet.mncrmtc.com/jennyh',
+  'databases' =>
+  array (
+    'default' =>
+    array (
+      'default' =>
+      array (
+        'driver' => 'mysql',
+        'database' => 'gonet_jennyh',
+        'username' => 'gonet',
+        'password' => 'NuhFQXrsrHYXeALkeCH6uXMp',
+        'host' => 'http://gonet.mncrmtc.com/',
+        'prefix' => '',
+        'collation' => 'utf8_general_ci',
+      ),
+    ),
+  ),
+);
+?>
